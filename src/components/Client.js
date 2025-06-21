4a46865c1263b1da1ca2506e0ce5ff0158787f38
devanshusingh76 <rajputdevanshu9084@gmail.com>
2025-06-21 14:45:44 +0530

    avatar changes

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import Avatar from 'react-avatar'
+import Avatar from 'boring-avatars'
 
-const Client = ({username}) => {
+const Client = ({ username }) => {
   return (
-    <div className='client'>
-        <Avatar 
-            name={username}
-            size={50}
-            round="14px"
-        />
-        <span className='username'>{username}</span> 
+    <div className="client">
+      <Avatar
+        size={50}
+        name={username}
+        variant="beam"
+        colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
+      />
+      <span className="username">{username}</span>
     </div>
-  )
-}
+  );
+};
 
 export default Client
